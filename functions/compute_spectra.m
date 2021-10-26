d3b00e362dd6967c3e1a1d067525a92f1ed6cfa6
Letitia Ho <letitiayhho@gmail.com>
2021-10-25 21:07:20 -0500

    Clean up script

--- functions/compute_spectra.m
+++ functions/compute_spectra.m
@@ -1,34 +1,57 @@
-function [secs, file_order] = compute_vowel_spectra()
-    cd('/Users/letitiaho/src/speeded_target_recognition_task')
-    paths = dir('stim/*/*.wav');
+function compute_peaks_for_all_ABR()
+    condition = "40BPM";
+    cd('/Users/letitiaho/src/serena/Movement FFR_All Analyses/')
+    paths = dir('3Blocks_Data/*/*' + condition + '*.wav');
     secs = zeros(length(paths), 1);
     file_order = [];
-    for i = 1:3
+    
+    % Initialize empty data arrays
+    peak = [];
+    subject = [];
+    condition = [];
+    segment = [];
+    
+    for i = 1:length(paths)
         filepath = fullfile(paths(i).folder, paths(i).name);
         
         % Extract file information
-        path_parts = split(paths(1).folder, '/');
-        subject = char(path_parts(end));
-        [~,vowel,~]  = fileparts(filepath);
-        vowel = vowel(1:2);
+%         path_parts = split(paths(1).folder, '/');
+%         subject = char(path_parts(end));
+%         subject = subject(end-1:end)
+        [~, filename, ~] = fileparts(filepath);
+        
+        % parse filename into details
+        filename = split(filename, '_');
+        this_subject = char(filename(1));
+        subject(i,:) = string(this_subject(end-1:end));
+        this_condition = char(filename(2));
+        condition(i,:) = string(this_condition(1:2));
+        this_segment = char(filename(3));
+        segment(i,:) = string(this_segment(end));
         
         % Read file and compute frequency spectra
-        [y, fs] = audioread(filepath);
-        n = length(y);          % number of samples
-        y = fft(y);             % fft
-        f = (0:n-1)*(fs/n);     % frequency range
-        power = abs(y).^2/n;    % power of the DFT
+        [f, power] = compute_power_spectrum(filepath)
         
-        figure(1)
-        subplot(2, 2, i)
-        plot(f,power)
-        title([subject, '/', vowel])
-        xlim([0 2000])
-        xlabel('Frequency')
-        ylabel('Power')
+        % Get peak at 440 Hz
+        indexes = f > 430 & f < 450;
+        peak(i,:) = max(power(indexes));
         
-%         subplot(2, 2, i+2)
-%         spectrogram(y, 100, 1, 10000, fs, 'yaxis');
-%         title(files(i))
+        % Plot
+%         filename = split(filename, '_');
+%         filename = strjoin(filename, "-");
+%         figure(1)
+%         subplot(5, 2, i)
+%         plot_frequency_spectrum(f, power)
+%         title(string(filename))
     end
+end
+
+table(subject, condition, segment, peak)
+
+
+function plot_frequency_spectrum(f, power)
+    plot(f, power)
+    xlim([0 1000])
+    xlabel('Frequency')
+    ylabel('Power')
 end
