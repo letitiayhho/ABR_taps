3686dcc4b4254ca78726cb8e0db425488ec72688
Letitia Ho <letitiayhho@gmail.com>
2021-10-27 12:54:16 -0500

    Compute power with basic version first, if results of power computations don't work then switch

--- functions/compute_power_spectrum.m
+++ functions/compute_power_spectrum.m
@@ -1,8 +1,8 @@
-function [f, amplitude] = compute_power_spectrum(filepath)
+function [f, power] = compute_power_spectrum(filepath)
     [y, fs] = audioread(filepath);
     n = length(y);          % number of samples
 
-%     % simple frequency spectrum https://www.mathworks.com/matlabcentral/answers/251061-how-to-plot-frequency-spectrum-of-a-signal-in-matlab
+%     % simple frequency spectrum https://www.mathworks.com/help/matlab/math/basic-spectral-analysis.html
 %     y = fft(y);             % fft
 %     f = (0:n-1)*(fs/n);     % frequency range
 %     power = abs(y).^2/n;    % power of the DFT
@@ -19,21 +19,19 @@ function [f, amplitude] = compute_power_spectrum(filepath)
 %     [pxx, fs] = pwelch(y,'power');
 %     power = pow2db(pxx);
     
-    % Serena
-    T = 1/fs;
-    t = (0:n-1)*T;
+    %% compute power
+    % simple version
+    y = fft(y);             % fft
+    f = (0:n-1)*(fs/n);     % frequency range
+    power = abs(y).^2/n;    % power of the DFT
+    
+    % serena's version
+%     NFFT = 2^nextpow2(n); % Next power of 2 from length of y
+%     power = fft(y, NFFT)/n;
+%     f = fs/2*linspace(0,1,NFFT/2+1);
+%     f = (0:n-1)*(fs/n);
 
-    NFFT = 2^nextpow2(n); % Next power of 2 from length of y
-    Y = fft(y,NFFT)/n;
-    f = fs/2*linspace(0,1,NFFT/2+1);
+%     figure(2),plot(f(10:1000),2*abs(Y(10:1000))) 
 
-    figure(2),plot(f(10:1000),2*abs(Y(10:1000))) 
-
-    [B,IX] = sort(2*abs(Y(1:NFFT/2+1))); 
-    BFloor=0.01; %BFloor is the minimum amplitude value (ignore small values) 
-    amplitude=B(B>=BF3loor); %find all amplitudes above the BFloor 
-    f=f(IX(1+end-numel(amplitude):end)); %frequency of the peaks
-
-    f = f';
-    amplitude = amplitude';
+    plot_power_spectrum(f, power);
 end
--- functions/plot_power_spectrum.m
+++ functions/plot_power_spectrum.m
@@ -1,6 +1,6 @@
 function plot_power_spectrum(f, power)
-    plot(f, power)
-%     xlim([0 1000])
+    plot(f, power);
+    xlim([0 3500])
     xlabel('Frequency')
     ylabel('Power')
 end
